4f78c3b0f30844bc663a119bcd274ec913348b6d
Albert Chu <acmm@AlbertdeMac-mini.local>
2014-08-19 12:45:25 +0800

    Initial Commit

---
+++ ACSegmentedViewControllerTests/ACSegmentedViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  ACSegmentedViewControllerTests.m
+//  ACSegmentedViewControllerTests
+//
+//  Created by Albert Chu on 14/8/19.
+//  Copyright (c) 2014年 AC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ACSegmentedViewControllerTests : XCTestCase
+
+@end
+
+@implementation ACSegmentedViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
